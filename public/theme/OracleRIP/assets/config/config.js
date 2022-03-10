70c9f1e916c94553278950668e530153a7dbbd42
CreadwordGOD <aaliyahkqqmbtj@gmail.com>
2022-03-10 16:16:53 +0800

    hello v2board

---
+++ public/theme/OracleRIP/assets/config/config.js
@@ -0,0 +1,20 @@
+//exp: window.settings.planCheckout:{content:'欢度xxx 通用优惠码', coupon:'XJSW2022'},
+window.settings.planCheckout = {};
+//Don`t fucking change this window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
+window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
+// /plan 套餐展示页下方 FAQ
+window.settings.buyPlan_faq=[
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+    ];
+// /traffic 流量明细页上方 手风琴FQA
+window.settings.userTraffic_faq = [
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+        {question:'dd', answer:'22'},
+    ];
---
+++ public/theme/OracleRIP/dashboard.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>{{$title}}</title>
+    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.998f31aa.js?v={{$version}}"></script>
+    <link rel="modulepreload" href="/theme/{{$theme}}/assets/vendor.edff013b.js?v={{$version}}">
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.89fd8103.css?v={{$version}}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
+    <script>this.globalThis || (this.globalThis = this); window.global = window;</script>
+    <script>
+    window.settings = {
+        title: '{{$title}}',
+        version: '{{$version}}',
+        description: '{{$description}}',
+        crisp_id: '{{$crisp_id}}',
+    }
+    </script>
+     <script src="/theme/{{$theme}}/assets/config/config.js"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/i18n.js"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/zh-CN.js"></script>
+    <script src="/theme/{{$theme}}/assets/i18n/en-US.js"></script>
+</head>
+<body class="font-inter antialiased bg-gray-100 text-gray-600 sidebar-expanded">
+<div id="app"></div>
+</body>
+
+</html>
