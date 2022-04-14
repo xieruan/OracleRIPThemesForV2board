c3de4a2159ce11174e60b2e2547b2fd75a5db5e9
CreadwordGOD <aaliyahkqqmbtj@gmail.com>
2022-04-14 21:53:16 +0800

    Add a little shit

--- public/theme/OracleRIP/assets/config/config.js
+++ public/theme/OracleRIP/assets/config/config.js
@@ -2,10 +2,11 @@
 window.settings.planCheckout = {};
 //Don`t fucking change this window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
 window.settings.i18n=['zh-CN','en-US','ja-JP','vi-VN'];
+window.settings.footerEmail = 'v2board@v2board.com';
 // /plan 套餐展示页下方 FAQ
 window.settings.buyPlan_faq=[
         {question:'dd', answer:'22'},
-        {question:'dd', answer:'22'},
+        {question:'ddddd', answer:'22'},
         {question:'dd', answer:'22'},
         {question:'dd', answer:'22'},
         {question:'dd', answer:'22'},
--- public/theme/OracleRIP/dashboard.blade.php
+++ public/theme/OracleRIP/dashboard.blade.php
@@ -3,9 +3,8 @@
 <head>
     <meta charset="UTF-8" />
     <title>{{$title}}</title>
-    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.998f31aa.js?v={{$version}}"></script>
-    <link rel="modulepreload" href="/theme/{{$theme}}/assets/vendor.edff013b.js?v={{$version}}">
-    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.89fd8103.css?v={{$version}}">
+    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.6b721931.js?v={{$version}}"></script>
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.5a3968d8.css?v={{$version}}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
@@ -23,7 +22,7 @@
     <script src="/theme/{{$theme}}/assets/i18n/zh-CN.js"></script>
     <script src="/theme/{{$theme}}/assets/i18n/en-US.js"></script>
 </head>
-<body class="font-inter antialiased bg-gray-100 text-gray-600 sidebar-expanded">
+<body class="antialiased bg-[#f8f8f8] text-gray-600 sidebar-expanded">
 <div id="app"></div>
 </body>
 
