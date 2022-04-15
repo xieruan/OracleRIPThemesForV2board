e2b2436fff94d2daacb359e35c26ae77a83e0a94
CreadwordGOD <aaliyahkqqmbtj@gmail.com>
2022-04-15 16:54:07 +0800

    change css

--- public/theme/OracleRIP/dashboard.blade.php
+++ public/theme/OracleRIP/dashboard.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8" />
     <title>{{$title}}</title>
-    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.b1a4212b.js?v={{$version}}"></script>
+    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.fbd8baa7.js?v={{$version}}"></script>
     <link rel="modulepreload" href="/theme/{{$theme}}/assets/vendor.cc981c55.js?v={{$version}}">
-    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.4c72c17d.css?v={{$version}}">
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.82fa4641.css?v={{$version}}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
