fe8a5e8b302cdf96f38e7a88025acb39f3addba0
https://www.3byte.me <aaliyahkqqmbtj@gmail.com>
2022-07-29 11:20:16 +0800

    fix bug

--- OracleAiRIP/public/theme/OracleAIRIP/dashboard.blade.php
+++ OracleAiRIP/public/theme/OracleAIRIP/dashboard.blade.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <title>{{$title}}</title>
-    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.af7450cd.js?v={{$version}}"></script>
+    <script type="module" crossorigin src="/theme/{{$theme}}/assets/index.ccfe00dc.js?v={{$version}}"></script>
     <link rel="modulepreload" href="/theme/{{$theme}}/assets/vendor.cc981c55.js?v={{$version}}">
-    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.bcd66907.css?v={{$version}}">
+    <link rel="stylesheet" href="/theme/{{$theme}}/assets/index.cab8c956.css?v={{$version}}">
 
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,minimum-scale=1,user-scalable=no">
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700"> -->
